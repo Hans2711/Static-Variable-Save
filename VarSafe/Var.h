ea30f1ad423843c14355d7017ee1b6375c1ff382
Hans2711 <52494904+Hans2711@users.noreply.github.com>
2020-06-24 02:52:43 +0200

    Added a lot comments

--- VarSafe/Var.h
+++ VarSafe/Var.h
@@ -32,14 +32,15 @@ std::string getAllVar()//Reads the File into a string
 std::string GetVar(std::string VarName);
 void AddVar(std::string VarName, std::string VarContent)//Adds or updates a variable
 {
-    VarContent = encrypt(VarContent);
-    if (GetVar(VarName) != "Error")
+    VarContent = encrypt(VarContent);// Encrypts the content
+    if (GetVar(VarName) != "Error")//If the Variable name already exists
     {
         std::string OldVar = GetVar(VarName);
-        if (OldVar == VarContent)
+        if (OldVar == VarContent)//if the content that already exists are the same it will exit
         {
             return;
         }
+        //Replaces the old Variable's content with the new one and writes it to the File
         std::string allVar = getAllVar();
         allVar.replace(allVar.find(OldVar), OldVar.length(), VarContent);
         std::fstream Update(FilePath);
@@ -48,6 +49,7 @@ void AddVar(std::string VarName, std::string VarContent)//Adds or updates a vari
     }
     else
     {
+        //Adds the Variable's name and content in the proper format to the file
         std::string Togstr = VarName;
         Togstr += "=" + VarContent + "&";
         std::string allVar = getAllVar();
@@ -60,34 +62,34 @@ void AddVar(std::string VarName, std::string VarContent)//Adds or updates a vari
 std::string GetVar(std::string VarName)//Gets the value of a variable but encrypted
 {
     std::string allVar = getAllVar();
-    for (int i = 0; i < allVar.length(); i++)
+    for (int i = 0; i < allVar.length(); i++)//Loops though every character of the files Content
     {
-        if (allVar[i] == '&' || i == 0)
+        if (allVar[i] == '&' || i == 0)//if the character equals to & or if its the first character
         {
-            if(allVar[i] == '&')
+            if(allVar[i] == '&')//if if the character equals to & it adds 1 to i
                 i++;
             int EndName = 0;
             bool brk = true;
-            for(int j = i; j < allVar.length(); j++)
+            for(int j = i; j < allVar.length(); j++)//loops again though the content until it finds an =
             { 
                 if (allVar[j] == '=' && brk)
                 {
-                    EndName = j;
+                    EndName = j;//saves then spot in the string where the = is
                     brk = false;
                 }
             }
-            if (EndName == 0)
+            if (EndName == 0)//If it fails the search for the next = symbol it returns an error
             {
                 return "Error";
             }
             std::string Name = "";
-            for (int j = i; j < EndName; j++)
+            for (int j = i; j < EndName; j++)// Loops though the part of the file content where the Variable name is and stores the characters in a new string
             {
                 Name += allVar[j];
             }
             i = EndName + 1;
             brk = true;
-            for (int j = i; j < allVar.length(); j++)
+            for (int j = i; j < allVar.length(); j++)//Loops though antil it finds the next & symbol and safes the position of it
             {
                 if ((allVar[j] == '&' || j == allVar.length() - 1) && brk)
                 {
@@ -96,17 +98,17 @@ std::string GetVar(std::string VarName)//Gets the value of a variable but encryp
                 }
             }
             std::string Content = "";
-            for (int j = i; j < EndName; j++)
+            for (int j = i; j < EndName; j++)//Loops though the part where the Variable's Content is and stores it in a new string
             {
                 Content += allVar[j];
             }
-            if (Name == VarName)
+            if (Name == VarName)//If the Variable's name equals the one requested in the function's parameters
             {
-                return Content;
+                return Content;// returns the content of the variable
             }
         }
     }
-    return "Error";
+    return "Error";//Returns an error because no variable with that name got found
 }
 void Start(std::string EncryptionKey)//Starts the session by setting stuff like the current directory etc...
 {
@@ -117,9 +119,11 @@ void Start(std::string EncryptionKey)//Starts the session by setting stuff like 
     std::string tmp = std::string(buffer).substr(0, pos);
     tmp += "\\Var";
     FilePath = tmp;
+    //Checks if the File where the Variables are stored exists.
     std::ifstream f(FilePath.c_str());
     if (!f.good())
     {
+        //Then creates the file if it doesnt exists
         std::ofstream tmp(FilePath);
         tmp << "";
         tmp.close();
